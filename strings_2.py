72cf362c9cab9f065940d6eaf8da99d05d79fd79
Gabriel <gccomputacao@gmail.com>
2023-07-24 20:04:43 -0300

    print()

--- strings_2.py
+++ strings_2.py
@@ -1,6 +1,8 @@
 ''' 
 Aula do DevAprender Mini Curso de Strings
 https://www.youtube.com/watch?v=kvHe2Yp-FTI&list=PLnNURxKyyLIKX73U7hISjIY7T5KiNNLu_&index=8
+Aula do DevAprender Curso de Git e Github
+https://www.youtube.com/watch?v=kB5e-gTAl_s
 '''
 
-
+print('Manipulação de Strings!')
